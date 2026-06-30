ebb77fd2556be81f57755953ca33043e686ae551
tanawat2549 <tanawatbinuma.beer@gmail.com>
2026-06-30 15:44:12 +0700

    EndLecture-01

---
+++ main1.py
@@ -0,0 +1,8 @@
+print("hello")
+print("Information and Network Engineering")
+print("Tanawat You will be great")
+
+print("I'm Hungry")
+print('He ask me"Who got the book?"')
+
+print("I'm good \"I will do my best\"")
